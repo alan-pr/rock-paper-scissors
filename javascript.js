2c2ff9f714f2fc7807825c95d8a74c095cc96b14
Alan <alan.pr88@gmail.com>
2024-03-23 09:50:39 -0500

    Add GUI to the rock-paper-scissors game

--- javascript.js
+++ javascript.js
@@ -1,59 +1,84 @@
-let rounds;
-let currentRound = 1;
+const buttons = Array.from(document.querySelector(".buttons").childNodes);
+const playerScoreElement = document.querySelector(".player_score");
+const computerScoreElement = document.querySelector(".comp_score");
+const result = document.querySelector(".result");
+const resultDescription = document.querySelector(".result_description");
+const dialog = document.querySelector("dialog");
+
 let playerScore = 0;
 let computerScore = 0;
-let gameRunning = true;
 
-function playRound(playerSelection, computerSelection) {
-    let win;
-    playerSelection = String(playerSelection).charAt(0).toUpperCase() + String(playerSelection).slice(1).toLowerCase();
+playerScoreElement.textContent = playerScore;
+computerScoreElement.textContent = computerScore;
 
-    if (playerSelection == "Null")
-        return "Game aborted!";
+buttons.forEach(button => {
+    if (button.nodeName == "BUTTON") {
+        button.addEventListener("click", e => {
+            const player = button.className;
+            const computer = getComputerSelection();
 
-    while (!isInputValid(playerSelection)) {
-        playerSelection = prompt("Invalid input\nRock, Paper, Scissors?");
+            if (player == computer) {
+                result.textContent = "Tied!";
+                resultDescription.textContent = `Both are ${String(player).charAt(0).toUpperCase() + String(player).slice(1)}`;
+            } else {
+                declareRoundWinner(player, computer);
+                if (playerScore == 5 || computerScore == 5)
+                    resetGame((playerScore == 5) ? "Player" : "Computer");
+            }
+        });
     }
+});
 
-    if (playerSelection == computerSelection)
-        return `It's a tie! Both are ${playerSelection}`;
+dialog.lastElementChild.addEventListener("click", () => {
+    dialog.style.display = "none";
+    dialog.close();
+});
 
-    if (playerSelection == "Rock") {
-        win = (computerSelection == "Paper") ? false : true;
-    } else if (playerSelection == "Paper") {
-        win = (computerSelection == "Rock") ? true : false;
-    } else {
-        win = (computerSelection == "Rock") ? false : true;
-    }
-
-    playerScore += (win) ? 1 : 0;
-    computerScore += (!win) ? 1 : 0;
-
-    return (win) ? `You win! ${playerSelection} beats ${computerSelection}` : `You lose! ${computerSelection} beats ${playerSelection}`;
-}
-
-function isInputValid(input) {
-    return (input == "Rock") ? true : (input == "Paper") ? true : (input == "Scissors") ? true : false;
-}
-
-function isRoundsValid(input) {
-    return (input > 0) ? true : false;
-}
-
-function getComputerChoice() {
+function getComputerSelection() {
     const selection = Math.round(Math.random() * 10) % 3;
-    return (selection < 1) ? "Rock" : (selection < 2) ? "Paper" : "Scissors";
+    return (selection == 0) ? "rock" : (selection < 2) ? "paper" : "scissors";
 }
 
-function playGame() {
-    while (!isRoundsValid(rounds = Number(prompt("How many rounds do you want to play?"))));
-
-    while (gameRunning) {
-        console.log(`Round ${currentRound++}: ` + playRound(prompt("Rock, Paper, Scissors?"), getComputerChoice()));
-        gameRunning = (currentRound > rounds) ? false : true;
+function declareRoundWinner(player, computer) {
+    if (player == "rock") {
+        if (computer == "paper")
+            updateComputerScore(player, computer);
+        else
+            updatePlayerScore(player, computer);
+    } else if (player == "paper") {
+        if (computer == "rock")
+            updatePlayerScore(player, computer);
+        else
+            updateComputerScore(player, computer);
+    } else {
+        if (computer == "rock")
+            updateComputerScore(player, computer);
+        else
+            updatePlayerScore(player, computer);
     }
-
-    console.log(`Score: Player: ${playerScore} - Computer: ${computerScore} (${(playerScore == computerScore) ? "No one" : (playerScore > computerScore) ? "Player" : "Computer"} wins!)`);
 }
 
-playGame(5);
+function updatePlayerScore(player, computer) {
+    playerScoreElement.textContent = ++playerScore;
+    result.textContent = "You win!";
+    resultDescription.textContent = `${String(player).charAt(0).toUpperCase() + String(player).slice(1)} beats ${String(computer).charAt(0).toUpperCase() + String(computer).slice(1)}`;
+}
+
+function updateComputerScore(player, computer) {
+    computerScoreElement.textContent = ++computerScore;
+    result.textContent = "You lose!";
+    resultDescription.textContent = `${String(computer).charAt(0).toUpperCase() + String(computer).slice(1)} beats ${String(player).charAt(0).toUpperCase() + String(player).slice(1)}`;
+}
+
+function resetGame(winner) {
+    dialog.style.display = "flex";
+    dialog.firstChild.textContent = `${winner} wins!`;
+    dialog.showModal();
+
+    playerScore = 0;
+    computerScore = 0;
+    playerScoreElement.textContent = playerScore;
+    computerScoreElement.textContent = computerScore;
+    result.textContent = "";
+    resultDescription.textContent = "";
+}
